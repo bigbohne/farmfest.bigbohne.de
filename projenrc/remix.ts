55f554bb5cc380b7f09de52831e7e0be8e63bc31
Bigbohne <bigbohne@gmail.com>
2025-03-28 23:32:08 +0000

    now build works

--- projenrc/remix.ts
+++ projenrc/remix.ts
@@ -1,57 +1,61 @@
-import { typescript, TextFile } from "projen";
-import { TypeScriptJsxMode, TypeScriptModuleResolution } from "projen/lib/javascript";
-import { TypeScriptProjectOptions } from "projen/lib/typescript";
+import { typescript, TextFile } from 'projen';
+import { TypeScriptJsxMode, TypeScriptModuleResolution } from 'projen/lib/javascript';
+import { TypeScriptProjectOptions } from 'projen/lib/typescript';
 
 export interface RemixProjectOptions extends TypeScriptProjectOptions { }
 
 export class RemixProject extends typescript.TypeScriptProject {
-    constructor(props?: RemixProjectOptions) {
-        super({
-            ...props!, tsconfig: {
-                ...props?.tsconfig, compilerOptions: {
-                    ...props?.tsconfig?.compilerOptions,
-                    module: 'es6',
-                    jsx: TypeScriptJsxMode.REACT_JSX,
-                    noEmit: true,
-                    types: ['@remix-run/node', 'vite/client'],
-                    skipLibCheck: true,
-                    moduleResolution: TypeScriptModuleResolution.BUNDLER,
-                    target: 'ES2022'
-                }
-            },
-            tsconfigDev: {
-                ...props?.tsconfigDev,
-                compilerOptions: {
-                    ...props?.tsconfigDev?.compilerOptions,
-                    module: 'commonjs'
-                }
-            },
-            srcdir: 'app',
-            sampleCode: false
-        });
+  constructor(props?: RemixProjectOptions) {
+    super({
+      ...props!,
+      tsconfig: {
+        ...props?.tsconfig,
+        compilerOptions: {
+          ...props?.tsconfig?.compilerOptions,
+          module: 'es6',
+          jsx: TypeScriptJsxMode.REACT_JSX,
+          noEmit: true,
+          types: ['@remix-run/node', 'vite/client'],
+          skipLibCheck: true,
+          moduleResolution: TypeScriptModuleResolution.BUNDLER,
+          target: 'ES2022',
+        },
+      },
+      tsconfigDev: {
+        ...props?.tsconfigDev,
+        compilerOptions: {
+          ...props?.tsconfigDev?.compilerOptions,
+          module: 'commonjs',
+        },
+      },
+      srcdir: 'app',
+      sampleCode: false,
+      eslint: false,
+    });
 
-        this.addDeps('@remix-run/node@2',
-            '@remix-run/serve@2',
-            '@remix-run/react@2',
-            'react@^18.0.0',
-            'react-dom@^18.0.0',
-            'isbot')
+    this.addDeps('@remix-run/node@2',
+      '@remix-run/serve@2',
+      '@remix-run/react@2',
+      'react@^18.0.0',
+      'react-dom@^18.0.0',
+      'isbot');
 
-        this.addDevDeps('@remix-run/dev@2',
-            '@types/react',
-            '@types/react-dom')
+    this.addDevDeps('@remix-run/dev@2',
+      '@types/react',
+      '@types/react-dom');
 
-        this.addTask('dev', {
-            exec: 'remix vite:dev'
-        })
+    this.addTask('dev', {
+      exec: 'remix vite:dev',
+    });
 
-        this.tasks.tryFind('pre-compile')?.exec('rm -rf ./build')
-        this.tasks.tryFind('compile')?.reset('npx remix vite:build')
+    this.tasks.tryFind('pre-compile')?.exec('rm -rf ./build');
+    this.tasks.tryFind('compile')?.reset('npx remix vite:build');
+    this.tasks.tryFind('package')?.reset();
 
-        this.gitignore.exclude('public/build', 'build')
+    this.gitignore.exclude('public/build', 'build');
 
-        const viteCOnfig = new TextFile(this, 'vite.config.mts');
-        viteCOnfig.addLine(`import { vitePlugin as remix } from "@remix-run/dev";
+    const viteCOnfig = new TextFile(this, 'vite.config.mts');
+    viteCOnfig.addLine(`import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import { installGlobals } from "@remix-run/node";
 
@@ -67,6 +71,6 @@ export default defineConfig({
       v3_throwAbortReason: true
     }
   })],
-});`)
-    }
+});`);
+  }
 }
